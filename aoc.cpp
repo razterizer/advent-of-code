d64464644f0c3b58e606c6c429098e7ca75a68cc
Rasmus Anthin <scratty@gmail.com>
2024-12-02 11:07:25 +0100

    Plugging in day2.

--- aoc.cpp
+++ aoc.cpp
@@ -6,11 +6,18 @@
 //
 
 #include "2024/day1/day1.h"
+#include "2024/day2/day2.h"
 #include <iostream>
 
 int main(int argc, char** argv)
 {
+  std::cout << "Day 1:" << std::endl;
   aoc_2024::day1();
+  std::cout << "------" << std::endl;
+  
+  std::cout << "Day 2:" << std::endl;
+  aoc_2024::day2();
+  std::cout << "------" << std::endl;
 
   return 0;
 }
