e0436735a2d7e3873300958d642a035815815a4c
Rasmus Anthin <scratty@gmail.com>
2024-12-02 09:48:55 +0100

    Added file day1 under folder 2024/day1 and adding main c++ file for all years and days to make XCode more happy and easy to work with.

---
+++ 2024/day1/day1.h
@@ -0,0 +1,12 @@
+//
+//  day1.h
+//  advent-of-code
+//
+//  Created by Rasmus Anthin on 2024-12-02.
+//
+
+#ifndef day1_h
+#define day1_h
+
+
+#endif /* day1_h */
---
+++ aoc.cpp
@@ -0,0 +1,8 @@
+//
+//  aoc.cpp
+//  advent-of-code
+//
+//  Created by Rasmus Anthin on 2024-12-02.
+//
+
+#include <stdio.h>
